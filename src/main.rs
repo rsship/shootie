d06814faaf5f079939456d7ae6d6d086f8b1919b
Salih Bozkaya <salihbozkaya@Salihs-MacBook-Pro.local>
2023-08-29 14:29:46 +0300

    centered coordinate system starting point instead of (0,0) default point

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use sdl2::event::Event;
 use sdl2::image::{self, InitFlag, LoadTexture};
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
-use sdl2::rect::Rect;
+use sdl2::rect::{Point, Rect};
 use sdl2::render::{Texture, WindowCanvas};
 
 const WINDOW_TITLE: &str = "shooter game";
@@ -39,6 +39,9 @@ fn main() -> Result<(), String> {
     let mut events = sdl_context.event_pump()?;
     let mut i = 0;
 
+    let position = Point::new(0, 0);
+    let sprite = Rect::new(0, 0, 26, 36);
+
     'running: loop {
         for event in events.poll_iter() {
             match event {
@@ -55,7 +58,14 @@ fn main() -> Result<(), String> {
 
         i = (i + 1) % 255;
 
-        render(&mut canvas, Color::RGB(i, 255, 255 - i), &texture)?;
+        update(&mut canvas);
+        render(
+            &mut canvas,
+            Color::RGB(i, 255, 255 - i),
+            &texture,
+            position,
+            sprite,
+        )?;
 
         std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
@@ -63,11 +73,25 @@ fn main() -> Result<(), String> {
     Ok(())
 }
 
-fn render(canvas: &mut WindowCanvas, color: Color, texture: &Texture) -> Result<(), String> {
+fn render(
+    canvas: &mut WindowCanvas,
+    color: Color,
+    texture: &Texture,
+    position: Point,
+    sprite: Rect,
+) -> Result<(), String> {
     canvas.set_draw_color(color);
     canvas.clear();
-    canvas.copy(texture, Rect::new(0, 0, 26, 26), Rect::new(0, 0, 26, 26))?;
+
+    let (ctx_w, ctx_h) = canvas.output_size()?;
+
+    let screen_center = position + Point::new((ctx_w / 2) as i32, (ctx_h / 2) as i32);
+    let screen_position = Rect::from_center(screen_center, sprite.width(), sprite.height());
+
+    canvas.copy(texture, sprite, screen_position)?;
     canvas.present();
 
     Ok(())
 }
+
+fn update(canvas: &mut WindowCanvas) {}
